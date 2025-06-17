56c61635935d113e12f4f189104f11b1395b3852
NguyenHungThinh204 <nguyenhungthinh2k4@gmail.com>
2025-06-17 11:22:53 +0700

    profile, edit profile ui

---
+++ app/src/main/java/com/example/btlandroid/ui/profile/EditProfileActivity.java
@@ -0,0 +1,15 @@
+package com.example.btlandroid.ui.profile;
+
+import android.os.Bundle;
+
+import com.example.btlandroid.R;
+import com.example.btlandroid.ui.BaseActivity;
+
+public class EditProfileActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}
---
+++ app/src/main/java/com/example/btlandroid/ui/profile/ProfileActivity.java
@@ -0,0 +1,15 @@
+package com.example.btlandroid.ui.profile;
+
+import android.os.Bundle;
+
+import com.example.btlandroid.R;
+import com.example.btlandroid.ui.BaseActivity;
+
+public class ProfileActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
